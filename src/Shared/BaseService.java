ee23e5373f5179b3ed1150a838ac8b96f97b501f
Fernando Di Lillo <di.lillo.fernando@gmail.com>
2019-01-17 09:21:46 +0100

    removed unused save all in repository and abstract service

--- src/Shared/BaseService.java
+++ src/Shared/BaseService.java
@@ -28,20 +28,9 @@ public interface BaseService extends Remote {
     JSONObject save(JSONObject data) throws Exception;
 
     /**
-     * Prepares multiple entities save operation
-     * @return (Result object, can be true or false and contains a message)
-     */
-    JSONObject saveAll(JSONObject data) throws Exception;
-
-    /**
      * Prepares a single entity delete operation
      * @return (Result object, can be true or false and contains a message)
      */
     JSONObject delete(JSONObject data) throws Exception;
 
-    /**
-     * Prepares multiple entities delete operation
-     * @return (Result object, can be true or false and contains a message)
-     */
-    JSONObject deleteAll(JSONObject data) throws Exception;
 }
